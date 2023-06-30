054d42e78136c800d6b5108538e932f984356073
Lexey9105 <umpp120@gmail.com>
2023-06-30 12:09:27 +0300

    Initial Commit

---
+++ src/main/java/pro/sky/StoreSky_21/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pro.sky.StoreSky_21;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
